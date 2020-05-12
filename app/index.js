67a4059f19dbf444b5a922829253ea0e92022f0d
Emily Morgado <emilyannemorgado@gmail.com>
2020-05-12 08:27:41 -0700

    Create App directory with starter JS and comments for learning

---
+++ app/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+// Components can have (are concerned with):
+// State
+// Lifecycle
+// UI
+
+// JSX is not violating the separation of concerns
+// because a component is concerned with UI
+
+// Babel converts JSX into JS createElement invocations
+
+class App extends React.Component {
+  render() {
+    // description of the UI
+    return (
+      <div>
+        Hello World!
+      </div>
+    )
+  }
+}
+
+// Takes two args:
+// the element,
+// where to render the element to
+
+// Is decoupled from React to allow you to
+// Use react for other environ (mobile app, xbox)
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
