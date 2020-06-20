f0d529c0affa6336cbf316bd34a540b350c3bf8b
Emily Morgado <emilyannemorgado@gmail.com>
2020-06-20 10:09:13 -0700

    Remove notes and import Battle component

--- app/index.js
+++ app/index.js
@@ -2,34 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import Popular from './components/Popular'
-
-// Components can have (are concerned with):
-// State
-// Lifecycle
-// UI
-
-// JSX is not violating the separation of concerns
-// because a component is concerned with UI
-
-// Babel converts JSX into JS createElement invocations
+import Battle from './components/Battle'
 
 class App extends React.Component {
   render() {
-    // description of the UI
     return (
       <div className="container">
-        <Popular />
+        <Battle />
       </div>
     )
   }
 }
 
-// Takes two args:
-// the element,
-// where to render the element to
-
-// Is decoupled from React to allow you to
-// Use react for other environ (mobile app, xbox)
 ReactDOM.render(
   <App />,
   document.getElementById('app')
